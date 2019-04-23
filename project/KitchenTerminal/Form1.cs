4589605475b2f1798c816ac81e5a0160bf43d692
Francisco Maria <franciscomaria97@hotmail.com>
2019-04-24 00:29:10 +0100

    Save received orders in kitchen terminal

--- project/KitchenTerminal/Form1.cs
+++ project/KitchenTerminal/Form1.cs
@@ -12,15 +12,13 @@ namespace KitchenTerminal
 {
     public partial class Form1 : Form
     {
+        List<Order> preparingOrders = new List<Order>();
+
         public Form1()
         {
-
             ConnectionObject.InitConnection();
             InitializeComponent();
-
-
             ConnectionObject.ReceiveNewOrder(receiveOrderFunc);
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,7 +29,17 @@ namespace KitchenTerminal
         public void receiveOrderFunc(Order newOrder)
         {
             Console.WriteLine("ORDER HAS ARRIVED. " + newOrder.getDestinationTable() + newOrder.getID());
+            substituteOrder(newOrder);
+            Console.WriteLine(preparingOrders.ToString());
         }
 
+        private void substituteOrder(Order newOrder)
+        {
+            int index = preparingOrders.FindIndex(ind => ind.Equals(newOrder));
+            if (index != -1)
+            {
+                preparingOrders[index] = newOrder;
+            }
+        }
     }
 }
